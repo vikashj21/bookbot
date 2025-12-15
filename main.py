830c3a0374fa4ff9582cb496d463426fb7d32d97
Vikash Jeyathevan <vjeyathevan@student.ethz.ch>
2025-12-15 23:41:01 +0100

    modified simple function for better readability

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
+from stats import count_words
+
 def get_book_text(filepath):
     print(filepath)
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
-def count_words(book):
-    return book.split()
-
-
 def main():
     # get_book_text('books/frankenstein.txt')
-    words = count_words(get_book_text('books/frankenstein.txt'))
-    print(f"Found {len(words)} total words")
+    num_words = count_words(get_book_text('books/frankenstein.txt'))
+    print(f"Found {num_words} total words")
 
 if __name__ == "__main__":
     main()    
